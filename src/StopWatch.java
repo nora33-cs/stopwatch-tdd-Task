777e64cd77e7ee432003ba9dc623a79dc98381c6
noran <zzznoura95@gmail.com>
2026-08-15 20:27:13 +0300

    TDD: ignore negative minutes

--- src/StopWatch.java
+++ src/StopWatch.java
@@ -3,8 +3,10 @@ public class StopWatch {
     int minutes;
 
     public void record(int minutes) {
+    if (minutes >= 0) {
         this.minutes = minutes;
     }
+}
 
     public int getMinutes() {
         return minutes;
