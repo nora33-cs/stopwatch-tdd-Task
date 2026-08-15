6be62f596fa232a43deb56d81c11a7c6a3583f47
noran <zzznoura95@gmail.com>
2026-08-15 20:18:55 +0300

    TDD: implement recording minutes

--- src/StopWatch.java
+++ src/StopWatch.java
@@ -1,4 +1,12 @@
 public class StopWatch {
 
-    
+    int minutes;
+
+    public void record(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
 }
